cf66c389bd38e79e9727fc5c4799745f0f243ced
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-12-13 14:36:41 +0200

    add dispatch action for adding books

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,19 +1,41 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../../redux/books/books';
 
-export default class Form extends PureComponent {
-  render() {
-    return (
-    // return jsx syntax for displaying in html
-      <>
-        <h3>ADD NEW BOOk</h3>
-        <form>
-          <input id="Title" type="text" placeholder="Book title" />
-          <input id="Author" type="text" placeholder="author" />
-          <button type="submit">ADD BOOK</button>
-        </form>
+const Form = () => {
+  const dispatch = useDispatch();
+  let INname;
+  let INauthor;
+  const handleIncremt = (name, author) => {
+    if (!name.value.trim() || !author.value.trim()) {
+      return;
+    }
+    const newBook = {
+      name: name.value,
+      author: author.value,
+      id: uuidv4(),
+    };
+    dispatch(addBook(newBook));
+    INname.value = '';
+    INauthor.value = '';
+  };
 
-      </>
+  return (
+  // return jsx syntax for displaying in html
+    <>
+      <h3>ADD NEW BOOk</h3>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        handleIncremt(INname, INauthor);
+      }}
+      >
+        <input ref={(node) => { INname = node; }} type="text" placeholder="Book title" />
+        <input ref={(node) => { INauthor = node; }} type="text" placeholder="author" />
+        <button type="submit" onClick={() => { handleIncremt(); }}>ADD BOOK</button>
+      </form>
+    </>
+  );
+};
 
-    );
-  }
-}
+export default Form;
