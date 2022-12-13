22a24f34f9bbbf4303f55c843d7eb3402549d1ce
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-12-13 16:37:09 +0200

    add useDispatch for catagories page

--- src/hooks/catagories/catagories.js
+++ src/hooks/catagories/catagories.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../../redux/catagories/catagories';
 // adding Catagories page for use
 export default function Catagories() {
+  const status = useSelector((state) => state.cat);
+  const dispatch = useDispatch();
   return (
     <>
-      <button type="button">Check status</button>
+      <button
+        type="button"
+        onClick={(e) => {
+          e.preventDefault();
+          dispatch(checkStatus());
+        }}
+      >
+        Check status
+
+      </button>
+      <p>{status}</p>
     </>
   );
 }
